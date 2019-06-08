43a8de06deddb4bb53a291c9e47c91c613e8c838
Mohammed Aslam <maslam15@in.ibm.com>
2019-06-08 15:09:11 +0530

    setup

---
+++ libs/ui-toolbar/src/lib/ui-toolbar.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '@workspace/material';
+
+import { ToolbarComponent } from './toolbar/toolbar.component';
+@NgModule({
+  imports: [
+    CommonModule,
+    MaterialModule
+  ],
+  declarations: [ToolbarComponent],
+  exports: [ToolbarComponent]
+})
+export class UiToolbarModule {}
